c80f801294377781b7b22e17167a76b36714d8fc
Ruy Junior <ruyjunior21@gmail.com>
2025-01-29 23:47:38 -0300

    Capitulo 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 //import postgres from 'postgres';
 import { sql } from '@vercel/postgres';
+import { error } from 'console';
 
 
 //const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
@@ -28,10 +29,14 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;    
     const date = new Date().toISOString().split('T')[0];  
     
-    await sql`
+    try {
+        await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;  
+        `;  
+    } catch (error){
+        console.error(error);
+    }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
@@ -44,18 +49,23 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
  
   const amountInCents = amount * 100;
- 
+ try {
   await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+ } catch (error){
+    console.error(error);
+ }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+    throw new Error('Failed to Delete Invoice');
+    
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
   }
