5ca332a905e44d0e3512987f0b698823f4e3f42c
Ruy Junior <ruyjunior21@gmail.com>
2025-02-03 03:02:35 -0300

    Conclusão

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,9 +5,30 @@ import { redirect } from 'next/navigation';
 //import postgres from 'postgres';
 import { sql } from '@vercel/postgres';
 import { error } from 'console';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 
 //const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 
 const FormSchema = z.object({
     id: z.string(),
