d65fbe16d400a5ff29b62281bf0901f0fcdb8bd7
Ruy Junior <ruyjunior21@gmail.com>
2025-02-28 16:57:15 -0300

    correção de imagem

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -1,10 +1,10 @@
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
-import logo from 'public/img/logo.png'
+import logo from 'public/img/logos/logo.jpg';
 
 export default function Logo() {
   return (
-    <div className={`${lusitana.className} flex flex-row items-center justify-center leading-none text-white`}>
+    <div className={`${lusitana.className} flex flex-row items-center justify-center leading-none text-white m-5`}>
       <Image src={logo} alt="AUTORIC" width={200} height={200} />
       <p className="text-[24px] m-2 p-0"></p>
     </div>
