d4869e1fd5bba6244c3289c1bec3e73bed62d786
Ruy Junior <ruyjunior21@gmail.com>
2025-03-10 08:17:44 -0300

    Incorporação Google Drive

--- app/ui/site/automation.tsx
+++ app/ui/site/automation.tsx
@@ -4,11 +4,20 @@ import { FaLink, FaGithub } from 'react-icons/fa';
 import { Project } from '@/app/lib/projects/definitions';
 
 export default function Automation() {
+    const folderId = "1LsKL_LmR3tjkCT0eyn3Imx6N3UyVriCO"
     return (
         <section id="automation" className="py-20 text-center">
             <h2 className="text-3xl font-bold mb-10">Automation Deliveries</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-4">
-                <p>Automation Deliveries</p>
+            <p>Here are some projects and services delivered.</p>
+            <div className="flex flex-col items-center justify-center bg-gray-100 px-1 py-1 rounded-md shadow-md">
+                <div className="flex justify-center">
+                    <iframe
+                        src={`https://drive.google.com/embeddedfolderview?id=${folderId}#list`}   //Use #grid or #list
+                        width="100%"
+                        height="300px"
+                        style={{ border: "none" }}
+                    ></iframe>
+                </div>
             </div>
         </section>);
 };
