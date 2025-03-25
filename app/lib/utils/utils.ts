05add95927178369b3fb60bcf430f4dffdc0f3cd
Ruy Junior <ruyjunior21@gmail.com>
2025-03-25 09:10:00 -0300

    Ajuste de funcção de data 2

--- app/lib/utils/utils.ts
+++ app/lib/utils/utils.ts
@@ -121,13 +121,10 @@ export const formatDateDb = (dateStr: string | null | undefined) => {
     return '';
   }
   const date = new Date(dateStr);
-  const timezoneOffset = date.getTimezoneOffset() * 60000;
-  /*const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;*/
-  const localISOTime = new Date(date.getTime() - timezoneOffset).toISOString().split('T')[0];
-  return localISOTime;
+  const year = date.getUTCFullYear();
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+  const day = String(date.getUTCDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
 };
 
 export const formatCurrencyInput = (value: string) => {
